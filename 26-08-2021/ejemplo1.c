316c0365aa159784ac3161fffc0f45b89f1d672e
tigarto <helpu1409@gmail.com>
2021-08-26 06:42:39 -0500

    Codigo del ejemplo 1 agregado

---
+++ 26-08-2021/ejemplo1.c
@@ -0,0 +1,41 @@
+/*
+Archivo: ejemplo1.c
+Fecha: 26/08/2021
+
+Enunciado: 
+Este programa sirve para calcular la serie Fibonacci.
+
+Comando de compilacion:
+gcc ejemplo1.c -o ejemplo1.out
+*/
+
+#include <stdio.h>
+
+
+/*
+return_type function_name (data type variable name list);
+}
+*/
+
+/* Declaracion de funciones */
+
+
+// Funcion de pruebas 
+void tests(void); 
+
+/* Funcion main */
+int main() {
+    // Variables locales
+
+
+    /* Codigo de la aplicacion */
+
+    /* 
+    Pruebas
+    tests(); Se comenta cuando las pruebas dan
+    */
+   
+    return 0;
+}
+
+/* Definicion de funciones */
